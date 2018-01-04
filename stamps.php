1330090eed05695cd7d0e49a61c845db3e959d46
PapamichMarios <papamichalopoulos.m@gmail.com>
2018-01-05 00:25:59 +0200

    Prosthiki pediwn gia autous pou den einai logged in

--- stamps.php
+++ stamps.php
@@ -18,6 +18,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 		<script src="scripts/backToTopButton.js"></script>
+		<script src="scripts/validateStamps.js"></script>
 	</head>
 
 	<body class="background-color">
@@ -36,29 +37,123 @@
       <a href="stamps.php"> <b> Ένσημα </b> </a>
     </h5>
 
-    <div class="well">
-      <h2 class="header text-center"> Παρουσίαση Ενσήμων </h2>
+		<?php
+		if ( isset( $_SESSION[ 'userid' ] ) )
+		{ ?>
+	    <div class="well">
+	      <h2 class="header text-center"> Παρουσίαση Ενσήμων </h2>
 
-      <div class="panel-group">
+	      <div class="panel-group">
 
-        <div class="panel panel-info">
-          <div class="panel-heading">Συνολικά Ένσημα</div>
+	        <div class="panel panel-info">
+	          <div class="panel-heading">Συνολικά Ένσημα</div>
 
-          <div class="panel-body">
-            <span class="col-md-2 attribute">420</span>
-          </div>
-        </div>
+	          <div class="panel-body">
+	            <span class="col-md-2 attribute">420</span>
+	          </div>
+	        </div>
 
-        <div class="panel panel-info">
-          <div class="panel-heading">Ένσημα προηγούμενου μήνα</div>
+	        <div class="panel panel-info">
+	          <div class="panel-heading">Ένσημα προηγούμενου μήνα</div>
 
-          <div class="panel-body">
-            <span class="col-md-2 attribute">4.20</span>
-          </div>
+	          <div class="panel-body">
+	            <span class="col-md-2 attribute">4.20</span>
+	          </div>
 
-        </div>
-      </div>
+	        </div>
+	      </div>
 
-    </div>
+	    </div>
+		<?php
+		}
+		else
+		{ ?>
+			<div class="well well-sm">
+				<h1 class="text-center header">Παρουσίαση ενσήμων </h1>
+				<div class="well well-sm">
+					<form class="form-horizontal" method="post" name="stampsForm" onsubmit="return validateStamps();">
+						<fieldset>
+
+							<p class="text-center footnote"> Παρακαλώ συπληρώστε όλα τα πεδία της φόρμας με ελληνικούς ή λατινικούς χαρακτήρες. Μπορείτε να παραλείψετε τους τόνους.
+								Αναγκαία η χρήση διαλυτικών όπου χρειάζεται.
+							</p>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-1 text-center"></span>
+								<div class="col-md-8">
+									<div class="input-group">
+									 <div class="input-group-addon">
+										 <span class="glyphicon glyphicon-user"></span>
+									 </div>
+									 <input id="fname" name="fname" type="text" placeholder="Όνομα" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-1 text-center"></span>
+								<div class="col-md-8">
+									<div class="input-group">
+									 <div class="input-group-addon">
+										 <span class="glyphicon glyphicon-user"></span>
+									 </div>
+									 <input id="lname" name="lname" type="text" placeholder="Επίθετο" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-1 text-center"></span>
+								<div class="col-md-8">
+									<div class="input-group">
+									 <div class="input-group-addon">
+										 <span class="glyphicon glyphicon-th-list"></span>
+									 </div>
+									 <input id="amka" name="amka" type="text" placeholder="Αριθμός Μητρώου Κοινωνικής Ασφάλισης" class="form-control" maxlength="11" onkeydown="return onlyNumbers(event);">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-1 text-center"></span>
+								<div class="col-md-8">
+									<div class="input-group">
+									 <div class="input-group-addon">
+										 <span class="glyphicon glyphicon-th-list"></span>
+									 </div>
+									 <input id="adt" name="adt" type="text" placeholder="Αριθμός Δελτίου Ταυτότητας" class="form-control" maxlength="8" >
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-1 col-md-offset-1 text-center"></span>
+								<div class="col-md-8">
+									<div class="input-group">
+									 <div class="input-group-addon">
+										 <span class="glyphicon glyphicon-calendar"></span>
+									 </div>
+									 <input id="bday" name="bday" type="text" placeholder="Ημερομηνία Γέννησης - HH/MM/EEEE" class="form-control">
+									</div>
+								</div>
+							</div>
+
+							<div class="form-group">
+								<span class="col-md-2 col-md-offset-2 text-center"></span>
+								<div class="col-md-6">
+									<button type="submit" class="btn btn-primary btn-md margin-buttons"><span class="glyphicon glyphicon-log-in"></span> Εγγραφή </button>
+									<button type="reset" class="btn btn-danger btn-md margin-buttons"><span class="glyphicon glyphicon-remove"></span> Εκκαθάριση Πεδίων </button>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+
+						<div id="displayOnError"> </div>
+
+				</div>
+			</div>
+			<?php
+		}
+		?>
   </body>
 </html>
