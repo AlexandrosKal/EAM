ad08607ad1082a2e0c950b623635e767611f1646
PapamichMarios <papamichalopoulos.m@gmail.com>
2018-01-05 00:24:44 +0200

    Prosthiki aitisewn gia suntaksi giratos kai anapirias

---
+++ application_pension.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<html lang="el">
+	<head>
+		<title>Αίτηση Συνταξιοδότησης</title>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<!-- NavBar css -->
+		<link rel="stylesheet" type="text/css" href="css/navbar.css">
+		<link rel="stylesheet" type="text/css" href="css/button.css">
+		<link rel="stylesheet" type="text/css" href="css/container.css">
+		<link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/navtabs.css">
+    <link rel="stylesheet" type="text/css" href="css/layout.css">
+		<link rel="stylesheet" type="text/css" href="css/error_text.css">
+		<link rel="stylesheet" type="text/css" href="css/signup_login.css">
+		<link rel="stylesheet" type="text/css" href="css/footnote.css">
+		<link rel="stylesheet" type="text/css" href="css/backToTopButton.css">
+    <link rel="stylesheet" type="text/css" href="css/well.css">
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+		<script src="scripts/backToTopButton.js"></script>
+	</head>
+
+	<body class="background-color">
+		<!--- include to navbar-->
+		<?php	require 'navbar.php' ;?>
+		<!-- include to koubi gia epistrofi stin korufi-->
+		<?php require 'backToTopButton.php' ;?>
+		<!--- set to path-->
+		<h5> <a href="index.php" class="padding"> <b> Αρχική Σελίδα </b></a>
+      >
+      <a href="services.php"> <b> Υπηρεσίες </b></a>
+      >
+      <a href="seniors.php"> <b> Συνταξιούχοι </b></a>
+      >
+      <a href="application_pension.php"> <b> Αίτηση Συνταξιοδότησης Γήρατος </b></a>
+    </h5>
+
+    <h2 class="page-header"> Αίτηση Συνταξιοδότησης Γήρατος</h2>
+
+    <div class="well">
+      <h3 class="text-center header"> Παρακαλώ ανεβάστε τα κατάλληλα δικαιολογητικά σε μορφή αρχείων .pdf.</h1>
+
+      <div class="well">
+        <form class="form-horizontal" method="post" name="loginForm" onsubmit="return validateApplicationPension()">
+          <fieldset>
+            <div class="form-group">
+              <span class="col-md-1 col-md-offset-1 text-center"></span>
+              <div class="col-md-8">
+                <div class="input-group">
+                 <div class="input-group-addon">
+                   <span class="	glyphicon glyphicon-folder-open"></span>
+                 </div>
+                 <input id="dikaiologitiko1" name="dikaiologitiko1" type="file" placeholder="Πόσες πούτσες έχετε φάει" class="form-control" accept="application/pdf">
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <span class="col-md-1 col-md-offset-1 text-center"></span>
+              <div class="col-md-8">
+                <div class="input-group">
+                 <div class="input-group-addon">
+                   <span class="	glyphicon glyphicon-folder-open"></span>
+                 </div>
+                 <input id="dikaiologitiko2" name="dikaiologitiko2" type="file" placeholder="Πόσες πούτσες έχετε φάει" class="form-control" accept="application/pdf">
+                </div>
+              </div>
+            </div>
+
+            <div class="form-group">
+              <div class="col-md-12 text-center">
+                <button type="submit" class="btn btn-primary btn-md"><span class="glyphicon glyphicon-log-in"></span> Υποβολή </button>
+                <button type="reset" class="btn btn-danger btn-md"><span class="glyphicon glyphicon-remove"></span> Εκκαθάριση Πεδίων </button>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+
+        <div id="displayOnError"> </div>
+
+      </div>
+    </div>
+
+  </body>
+</html>
---
+++ scripts/validateStamps.js
@@ -0,0 +1,110 @@
+function validateStamps()
+{
+  var errorList = [];
+  var error_flag = false;
+
+  /*briskoume ti lista me ta errors*/
+  var input = document.getElementById("fname").value;
+  if(input==="")
+  {
+    error_flag = true;
+    errorList.push("Παρακαλώ συμπληρώστε το πεδίο ονόματος.");
+  }
+  else
+  {
+    var inputArray = input.split(" ");
+    var regex = /(^([Α-Ω]|[ΆΈΉΊΌΎΏΪΫ])[α-ω]*([ϊϋ]?[α-ω]*[άέήόίύώ]?|[ΐΰ]?)[α-ω]*\s?$|^[A-Z][a-z]*\s?$)/;
+    for(var i=0; i<inputArray.length; i++)
+    {
+      if( !regex.test(inputArray[i]) )
+      {
+        error_flag=true;
+        errorList.push("To πεδίο ονόματος δεν είναι έγκυρο.");
+        break;
+      }
+    }
+  }
+
+  input = document.getElementById("lname").value;
+  if(input==="")
+  {
+    error_flag = true;
+    errorList.push("Παρακαλώ συμπληρώστε το πεδίο επιθέτου.");
+  }
+  else
+  {
+    if( !/(^([Α-Ω]|[ΆΈΉΊΌΎΏΪΫ])[α-ω]*([ϊϋ]?[α-ω]*[άέήόίύώ]?|[ΐΰ]?)[α-ω]*\s?$|^[A-Z][a-z]*\s?$)/.test(input) )
+    {
+      error_flag=true;
+      errorList.push("To πεδίο επιθέτου δεν είναι έγκυρο.");
+    }
+  }
+
+  input = document.getElementById("amka").value;
+  if(input==="")
+  {
+    error_flag = true;
+    errorList.push("Παρακαλώ συμπληρώστε το πεδίο αριθμού μητρώου κοινωνικής ασφάλισης.");
+  }
+  else
+  {
+    if(input.length != 11)
+    {
+      error_flag = true;
+      errorList.push("Παρακαλώ συμπληρώστε το πεδίο αριθμόυ μητρώου κοινωνικής ασφάλισης με 11 ψηφία.");
+    }
+  }
+
+  input = document.getElementById("adt").value;
+  if(input==="")
+  {
+    error_flag = true;
+    errorList.push("Παρακαλώ συμπληρώστε το πεδίο αριθμού δελτίου ταυτότητας.");
+  }
+  else
+  {
+    if(input.length != 8)
+    {
+      error_flag = true;
+      errorList.push("Παρακαλώ συμπληρώστε το πεδίο αριθμόυ δελτίου ταυτότητας με 8 ψηφία.");
+    }
+
+    if( !/^(?:[Α-Ω]){2}(?:[0-9]){6}$/.test(input) )
+    {
+      error_flag=true;
+      errorList.push("To πεδίο αριθμού δελτίου ταυτότητας δεν είναι έγκυρο. Μην χρησιμοποιείται κενό μεταξύ των αριθμών και των γραμμάτων.");
+    }
+  }
+
+  input = document.getElementById("bday").value;
+  if(input==="")
+  {
+    error_flag = true;
+    errorList.push("Παρακαλώ συμπληρώστε το πεδίο ημερομηνίας γέννησης.");
+  }
+  else
+  {
+    if( !/^(?:[0-9]){2}\/(?:[0-9]){2}\/(?:[0-9]){4}$/.test(input) )
+    {
+      error_flag=true;
+      errorList.push("To πεδίο ημερομηνίας γέννησης δεν είναι έγκυρο.");
+    }
+  }
+
+  /*ektupwsi tis listas*/
+  if(error_flag === true)
+  {
+    document.getElementById("displayOnError").innerHTML="";
+    for(var i=0; i < errorList.length; i++)
+    {
+        document.getElementById("displayOnError").innerHTML += "<span class='col-md-1 col-md-offset-1 text-center'></span>";
+        document.getElementById("displayOnError").innerHTML += "<li class='text-danger'> <span class='glyphicon glyphicon-alert'></span> &nbsp;" + errorList[i] + "</li>";
+    }
+
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
