a85129f21ff759280a50c4e9db4c90407fa912cf
PapamichMarios <papamichalopoulos.m@gmail.com>
2018-01-06 22:23:44 +0200

    Metafora arxeiwn sta views

--- backToTopButton.php
+++
@@ -1,4 +0,0 @@
-<button onclick="topFunction()" id="topButton" title="Επιστροφή στην κορυφή.">
-  <span class="glyphicon glyphicon-collapse-up"> </span>
-  Κορυφή
-</button>
--- printButton.php
+++
@@ -1,3 +0,0 @@
-<button class="btn btn-primary" id="printButton" type="button" onclick="printButton()">
-  Εκτύπωση
-</button>
