6b9fec715904c620b3efa0e291ca2fab2887f118
Alexandros Kalimeris <kalimerisalx@gmail.com>
2018-01-07 00:07:11 +0200

    Fix cookie deletion

--- logout.php
+++ logout.php
@@ -1,11 +1,11 @@
 <?php
-    require 'models/connect.php';
     require 'models/users.php';
+    require 'models/connect.php';
     if ( isset( $_SESSION[ 'uid' ] ) ) {
         session_destroy();
         if (isset($_COOKIE['uid_ika'])) {
             unset($_COOKIE['uid_ika']);
-            setcookie('uid_ika', '', time() - 3600, '/'); // empty value and old timestamp
+            setcookie('uid_ika', '', time() - 3600); // empty value and old timestamp
         }
     }
     header('Location: index.php' );
