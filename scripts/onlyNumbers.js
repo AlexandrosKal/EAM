37e6ee8d35b7b32e56020814e859cd1ef7e228e3
PapamichMarios <papamichalopoulos.m@gmail.com>
2018-01-07 22:26:32 +0200

    Fix to tab gia onlyNumber pedia

--- scripts/onlyNumbers.js
+++ scripts/onlyNumbers.js
@@ -2,5 +2,5 @@ function onlyNumbers(event)
 {
   var charCode = (event.which) ? event.which : event.keyCode;
 
-  return (charCode >= 48 && charCode <= 57) || charCode == 8 || charCode == 46;
+  return (charCode >= 48 && charCode <= 57) || charCode == 8 || charCode == 46 || charCode == 9;
 }
